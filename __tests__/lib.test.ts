c0caac31367869cad577cd35617d34acb14b1881
Sebastian Zentgraf <szentgraf@googlemail.com>
2024-11-15 17:18:15 +0100

    remove pdf tests

--- __tests__/lib.test.ts
+++ __tests__/lib.test.ts
@@ -1,13 +1,13 @@
-import { describe, expect, test } from '@jest/globals';
-import arraySpliceReorder from '../lib/arraySpliceReorder';
+import { describe, expect, test } from "@jest/globals";
+import arraySpliceReorder from "../lib/arraySpliceReorder";
 
-describe('reorder strings', () => {
-  test('reoder 3 items', () => {
-    const arrayStrings = ['1', '2', '3'];
+describe("reorder strings", () => {
+  test("reoder 3 items", () => {
+    const arrayStrings = ["1", "2", "3"];
     const reorder = arraySpliceReorder(arrayStrings, [-1, 0]);
-    expect(reorder).toEqual(['3', '1', '2']);
+    expect(reorder).toEqual(["3", "1", "2"]);
   });
-  test('reoder 0 items', () => {
+  test("reoder 0 items", () => {
     const arrayStrings: string[] = [];
     const reorder = arraySpliceReorder(arrayStrings, [-1, 0]);
     expect(reorder).toEqual([]);
